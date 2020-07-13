2c3afaff17dd92e4262b12b18a343bb3b4708a8b
Taylor Jovi <taylorljovi@gmail.com>
2020-07-12 21:59:14 -0500

    onto Part 4

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
 import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -31,6 +33,7 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers",employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -43,8 +46,11 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-
-        return "redirect:";
+        Optional optEmployer = employerRepository.findById(employerId);;
+        if (optEmployer.isPresent()) {
+            newJob.setEmployer((Employer)optEmployer.get());
+        }
+            return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
